df2a002587623e4ca51f50b8e9067faa158dd11b
StephenGrable1 <stephengrable@gmail.com>
2018-01-09 13:14:21 -0500

    init setup

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
+import './App.css';
+import Projects from './Projects/Projects.js'
+import Bio from './Bio/Bio.js'
+import Home from './Home/Home.js'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/bio'>Bio</Link></li>
+            <li><Link to='/projects'>Projects</Link></li>
+          </ul>
+
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/bio' component={Bio}/>
+          <Route exact path='/projects' component={Projects}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/Bio/Bio.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Bio.css';
+
+class Bio extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Bio</h3>
+      </div>
+    );
+  }
+}
+
+export default Bio;
