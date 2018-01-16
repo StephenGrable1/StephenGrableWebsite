b1f360649b0f8b04204ab616c0a79e1b441ed821
StephenGrable1 <stephengrable@gmail.com>
2018-01-16 13:04:46 -0500

    write marketing material

--- src/Home/IconMarketing/IconMarketing.js
+++ src/Home/IconMarketing/IconMarketing.js
@@ -22,27 +22,35 @@ class IconMarketing extends Component {
           <div className="icon-section-inner">
             <div className="image-outer">
               <img src={analytics} alt="analytics"></img>
+              <h3 className="marketing-title">Grow Your Business</h3>
+
+
               <p className="marketing-text">
-              This is the marketing text and I am makeing it longer
+              Reach more customers, serve more clients and grow your business through a new website.
               </p>
             </div>
             <div className="image-outer">
               <img src={calendar} alt="calendar"></img>
+              <h3 className="marketing-title">Meet your budget goals.</h3>
+
               <p className="marketing-text">
-              This is the marketing text
+               I am a FullStack developer who can save you the time and hassle of a full team.
               </p>
             </div>
             <div className="image-outer">
               <img src={responsive} alt="responsive screens"></img>
+              <h3 className="marketing-title">Design</h3>
               <p className="marketing-text">
-              This is the marketing text
+              Visitors expect your website to look good on every screen.
+              My website designs will help you look great at any screen size.
+              (Flip your phone to the side or resize your browser to see it in action)
               </p>
             </div>
           </div>
         </div>
 
         <div className="icon-marketing-action-btn">
-          <Link to="/contact" className="icon-marketing-link">Contact Me</Link>
+          <Link to="/contact" className="icon-marketing-link">Request a Quote</Link>
         </div>
 
       </div>
