ba76ddf4cf5193ebc226558b84683a4444f19c72
StephenGrable1 <stephengrable@gmail.com>
2018-02-05 15:19:54 -0500

    spell check written copy for deafboy site and tweak styles

--- src/components/Projects/DeafBoyWebsite/DeafBoyWebsite.js
+++ src/components/Projects/DeafBoyWebsite/DeafBoyWebsite.js
@@ -15,7 +15,7 @@ class DeafBoyWebsite extends Component {
     return (
         <div className="deaf-boy-outer">
         <div className="deaf-boy-inner">
-          <h3 className="project-title-db">deafBoy USA Email Singup Landing Page</h3>
+          <h3 className="project-title-db">deafBoy USA Landing Page</h3>
           <img src={deafBoyPreview} alt="computer-preview-db" className="computer-preview-db"></img>
           <p className="deaf-boy-intro">
           Email subscription with Drip marketing integration
@@ -40,10 +40,10 @@ class DeafBoyWebsite extends Component {
               <p>
               <a href="https://www.getdrip.com/">Drip email marketing</a> is one of the most advanced email automation software in the market today. I wanted to build
               a landing page that would enable users to easily sign up to a email newsletter. I also wanted to integrate a reCaptcha from Google to prevent spam and bots
-              from accessing and singing up for the newsletter. 
+              from accessing and signing up for the newsletter. 
               </p>
               <p>
-              Users always comes first. So, I decided to impliment a progress bar to update the user on their progress while entering and submitting their email.
+              Users always comes first. So, I decided to implement a progress bar to update the user on their progress while entering and submitting their email.
               As soon as the user starts to type their email, the progress bar responds and gives them immediate feedback. This feature turns a simple email signup form
               into a game. A game that the user is compelled to finish. 
               </p>
@@ -53,7 +53,7 @@ class DeafBoyWebsite extends Component {
               </p>
 
               <p>
-              Software should respond immediately and communicate with the user in real time at every click and key stroke.
+              Software should respond immediately and communicate with the user in real time at every click and keystroke.
               </p>
 
               <p>
