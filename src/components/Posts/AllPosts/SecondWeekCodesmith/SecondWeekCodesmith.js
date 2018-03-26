f15259b2e34a1e75bb7b73f265729b87e2cb39d1
StephenGrable1 <stephengrable@gmail.com>
2018-03-26 12:33:46 -0400

    style the container to new height for added text

--- src/components/Posts/AllPosts/SecondWeekCodesmith/SecondWeekCodesmith.js
+++ src/components/Posts/AllPosts/SecondWeekCodesmith/SecondWeekCodesmith.js
@@ -29,28 +29,33 @@ const SecondWeekCodesmith = () => {
 
                     <br />
                     <br />
-                    <h3>Project demos by the seniors.</h3>
+                    <h3>Senior Project Demos.</h3>
                     This week we saw the senior residents, who are 6 weeks ahead of our cohort, 
-                    present their final projects. Seniors have only been working on these projects
+                    present the first prototype of their final projects. Seniors have only been working on these projects
                      for 2 weeks and already have working prototypes. They've built <a href="https://reactjs.org/docs/state-and-lifecycle.html">React lifecycle </a> 
                      visualizer tools,<a href="https://graphql.org/"> GraphQL </a> query tools and an addon for 
-                     docker containers. Still haven't a clue for a good idea for my final project... will check back soon.
+                     docker containers. Still haven't a clue for a good idea for my final project... hopefully some divine inspiration will hit me soon.
 
                     <br />
                     <br />
-                    <h3>Week Overview.</h3>
-                    We had to absorb a lot of information this week. Everything from 
-                    <a href="https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol"> HTTP requests</a> to 
+                    <h3>2nd Week Overview.</h3>
+                    This week we covered everything from <a href="https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol"> HTTP requests</a> to 
                     asynchronous Javascript and the event loop. Most of the great learning moments came from paired programming 
-                    and implementing concepts we learned in lectures into working web pages. There was a lot of material covered 
-                    this week and because we are moving fast, it takes discipline to understand the concepts fully
-                    before we have to move on. 
+                    and implementing concepts we learned in lectures into working web pages. There was a ton of material covered 
+                    this week. 
+                    <br/>
+                    <br/>
+                    
+                    Since we are moving so quickly through material, I've found it imperative to understand the current 
+                    topics before moving on. This might mean staying late and putting in extra time to fully grasp concepts. 
+                    My cohort usually doesn't leave the Codesmith office until after 9:30pm. Putting in significant time 
+                    is necessary to succeed in this endeavour. If you do decide to attend Codesmith, you should be prepared, and excited, for these long days.
+                     
                     
                     <br/>
                     <br/>
                     
-                    Codesmith is a marathon. You have to be willing to put in the long hours
-                    necessary to fully absorb the material. It's not easy. I am fairly impressed with my personal growth so far.
+                    I am fairly impressed with my personal growth so far.
                     By the end of these 12 weeks I am confident I will have leveled up as a software engineer.
                     Overall, the material itself feels very relevant and I enjoy how the weekly challenges push me to employ skills that 
                     are beyond my comfort zone.
