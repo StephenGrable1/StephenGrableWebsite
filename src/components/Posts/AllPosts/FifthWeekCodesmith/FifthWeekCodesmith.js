345a8971557703a0912b2639596dad20012ab936
StephenGrable1 <stephengrable@gmail.com>
2018-04-15 18:01:48 -0400

    add gifs and write more content for week 5

--- src/components/Posts/AllPosts/FifthWeekCodesmith/FifthWeekCodesmith.js
+++ src/components/Posts/AllPosts/FifthWeekCodesmith/FifthWeekCodesmith.js
@@ -1,54 +1,110 @@
 import React, { Component } from 'react';
 import './FifthWeekCodesmith.css';
 
+import OmegaSpacePre from './media/OmegaSpace.gif';
+import audioAuction from './media/audioAuction.gif';
+
 class FifthWeekCodesmith extends Component {
-    render(){
+    render() {
         return (
             <div className="article-fifth-week">
                 <div className="fifth-week-outer">
                     <h1>My 5th week at Codesmith. Full stack apps. Tying it all together.</h1>
                     <h3>Work hard, play hard.</h3>
-                        <div className="fifth-week-text">
-                            This is where it starts to get exciting. The fifth week of Codesmith is all about 
-                            building projects that bring together all of the past technologies you've been working 
-                            with in the last four weeks. The Codesmith team encourages you to build full stack 
+                    <div className="fifth-week-text">
+                        This is where it starts to get exciting. The fifth week of Codesmith is all about
+                            building projects that bring together all of the past technologies you've been working
+                            with in the last four weeks. The Codesmith team encourages you to build full stack
                             applications that use both front end technologies, like React, and backend databases.
-                            
+
                             <br />
-                            <br />
-                            At beginning of this week, residents in my cohort built a plethora of really cool solo projects. This is one of 
-                            the few times during the program where you work alone. The guidelines for this portion of the week were 
+                        <br />
+                        At beginning of this week, residents in my cohort built a plethora of really cool solo projects. This is one of
+                            the few times during the program where you work alone. The guidelines for this portion of the week were
                             very open ended, which meant you could work on really whatever you wanted.
-                             Residents in my cohort built micro-blogging websites, real-time collaborative drawing applications and many other cool and interesting 
+                             Residents in my cohort built micro-blogging websites, real-time collaborative drawing applications and many other cool and interesting
                             projects.
 
                             <br />
-                            <br />
-                            For my solo project, I built a full stack auction application. The idea was simple. I wanted to create 
-                            a auction site for audio instrumentals. The auction time would randomly be set anywhere between five minutes 
-                            and 24 hours. Once the current auction ended, I would randomly set the next auctions end time in the database 
-                            and also randomly select six tracks to become available for purchase from a pool of around 100 other audio tracks. 
+                        <br />
+                        For my solo project, I built a full stack auction application. The idea was simple. I wanted to create
+                            a auction site for audio instrumentals. The auction time would randomly be set anywhere between five minutes
+                            and 24 hours. Once the current auction ended, I would randomly set the next auctions end time in the database
+                            and also randomly select six tracks to become available for purchase from a pool of around 100 other audio tracks.
 
                             <br />
-                            <br />
-                            In two days, I managed to create a countdown clock on the front end using React.
-                            I also connected this clock to my PostgreSQL database to read the auction end time 
-                            and randomly set the next 6 audio tracks to be displayed in the next auction once 
-                            the current one terminated. 
+                        <br />
+                        In two days, I managed to create a countdown clock on the front end using React.
+                            I also connected this clock to my PostgreSQL database to read the auction end time
+                            and randomly set the next 6 audio tracks to be displayed in the next auction once
+                            the current one terminated.
 
                             <br />
-                            <br />
-                            I encountered a few technological challenges along the way. Before this project, I had little 
-                            experience working with PostgreSQL databases. When I began implementing the backend for my app, I went back 
-                            and forth deciding how I would handle the random auction durations. First I tried to do it with PostgreSQL triggers.
-                            However, when I tried to implement that strategy, I encountered some roadblocks. First of all, triggers are 
-                            great when you want to automate database logic before or after a certain action takes place. This makes it easy to scale 
-                            your application to different platforms without copy and pasting the same database logic everywhere because your logic 
-                            lives in the database itself. This is a really cool feature of PostgreSQL. I ended up scratching this approach because 
-                            handling randomly generated times was much easier to do in my Express server than it was to do in PostgreSQL. However, I 
-                            now am more comfortable with PostgreSQL and have a better sense of when and how to use triggers to automate database logic. 
+                        <br />
 
-                           
+                        <div className="audio-auction-outer">
+                            <div className="audio-auction-inner">
+                                <img src={audioAuction} alt="Audio auction preview with countdown clock" />
+                                <p>Here is the demo of the auction app I built in just 2 days.
+                                    I'm manually resetting the auction here to demonstrate how
+                                    the app would work when the auction timer hits zero.
+                                </p>
+                            </div>
+                        </div>
+
+                        <br />
+                        <br />
+                        I encountered a few technological challenges along the way. Before this project, I had only a handful of
+                            experiences working with PostgreSQL databases. When I began implementing the backend for my app, I went back
+                            and forth deciding how I would handle the process of randomly selecting the auction duration. First I tried to do it with PostgreSQL triggers.
+                            However, when I tried to implement that strategy, I encountered some roadblocks.
+
+                            <br />
+                        <br />
+                        First of all, triggers are
+                            great when you want to automate database logic before or after a certain action takes place. This makes it easy to scale
+                            your application to different platforms without copy and pasting the same database logic everywhere because your logic
+                            lives in the database itself. This is a really cool feature of PostgreSQL. I ended up scratching this approach because
+                            handling randomly generated times was much easier to do in my Express server than it was to do in PostgreSQL. However, during my 
+                            research into PostgreSQL triggers, I gained better sense of how and when to use triggers to automate and simplify your database logic.
+
+                        <h3>Project #2. Team work flow and coordination.</h3>
+                        During the second half of the week, we were grouped into teams of 3 to 4 people. Each team had to iterate on 
+                        ideas and present 2 to 3 options to the Codesmith fellows. This process was more structured than the solo project. 
+                        I'm glad it was because during our pitch, the fellows helped us scope our project into something we could finish 
+                        in 3 days. 
+
+                        <br />
+                        <br />
+                        My team and I began the 3 day sprint with the idea to create a <a href="http://csbin.io/">Csbin.io</a> clone. 
+                        A few hours into the project and we decided to re-scope our project create something more similar to Google Docs instead of Csbin.
+                        Instead executing Javascript in our text editor, we wanted to focus more on a collaborative note-pad where users could simply edit a document at the same time. 
+
+                        <br />
+                        <br />
+
+                        <div className="omega-space-outer">
+                            <div className="omega-space-inner">
+                                <img src={OmegaSpacePre} alt="Collaborative note-pad app that lets users edit the same document in real-time" />
+                                <p>This is a working demo of the real-time collaborative document editor my team and I build 
+                                    in a little under 3 days. 
+                                </p>
+                            </div>
+                        </div>
+
+                        <br />
+                        <br />
+                        There were a couple of interesting technological challenges in this build. You can head over to <a href="https://github.com/StephenGrable1/OmegaSpace">the repository for this
+                        project</a> to read how my team and I overcame these challenges and went on to present a fully functional demo of our app at the end of the week. 
+
+                        <br />
+                        <br />
+
+                        <h3>Team collaboration and workflow</h3>
+                        One of the main things I learned during this project was how to work effectively with a team to cordinate efforts and 
+                        meet deadlines. We had a team of 3 developers and we began the week by outlining all of the tasks needed to be done 
+                        in order for use to have a working product by the end of 3 days. 
+
                         </div>
                 </div>
             </div>
