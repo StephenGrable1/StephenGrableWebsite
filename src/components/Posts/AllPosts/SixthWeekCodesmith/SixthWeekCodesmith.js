35d26d79c6638cf8528536ad27b2f1d9a6a1570b
StephenGrable1 <stephengrable@gmail.com>
2018-04-22 19:32:30 -0400

    tweaks to video

--- src/components/Posts/AllPosts/SixthWeekCodesmith/SixthWeekCodesmith.js
+++ src/components/Posts/AllPosts/SixthWeekCodesmith/SixthWeekCodesmith.js
@@ -11,31 +11,31 @@ class SixthWeekCodesmith extends Component {
                     <h1>Made it to halftime. 6 weeks in.</h1>
                     <h3>Pickup someone else's project and make it better.</h3>
                     <div className="sixth-week-text">
-                        This week was a roller-coaster. We started this week with a new preassigned group.
+                        This week was a roller-coaster.
                            The first thing we had to decide was which project we would choose to build on top of.
                            Last week, our cohort made 4 full stack applications. My group created
                            <a href="https://github.com/StephenGrable1/OmegaSpace"> a collaborative document editor
-                           using Socket.io, React and PostgreSQL.</a> This week, we changed groups and
-                           chose which application we wanted to built on top of.  This would teach us how to find
-                           our way through a unfamiliar codebase.
+                           using Socket.io, React and PostgreSQL.</a> We changed groups and
+                           decided which application we wanted to improve.  Our goal was to learn how to find and improve
+                           an unfamiliar codebase.
 
                             <br />
                         <br />
                         My group and I picked a project that was very challenging. We went into the week knowing
                             that our codebase wasn't working. A risky move, since we only had three days to make our improvements.
-                            Our choice was different from the other groups decisions to pickup a project that was working from the start.
+                            Our choice was different from the other groups who decided to pickup a project that was working from the start.
 
                             <br />
                         <br />
                         <h3>Multiplayer Asteroid Game in the browser.</h3>
                         The group that originally built the first version of the code
                             designed it to be played over websockets. The architecture employed some advance
-                            <a href="https://isaacsukin.com/news/2015/01/detailed-explanation-javascript-game-loops-and-timing#interpolate-drawing"> state management via interpolation. </a>
-                        It took much of the 3 days just understanding this subject. Interpolation helps you predict what a game would look
+                            <a href="https://isaacsukin.com/news/2015/01/detailed-explanation-javascript-game-loops-and-timing#interpolate-drawing"> state management techniques for smooth graphics. </a>
+                        It took much of the 3 days just to understand these techniques. Interpolation helps you predict what a game would look
                             like at any given time. When building a multi-player game, you can't rely on the network to send information
-                            instantly to all clients. Because of network latency, clients would experience smooth gameplay only dependent on their connection speed. This would most definitely cause glitchy gameplay.
+                            instantly to all clients. This would most definitely cause glitchy gameplay.
                             You also can't rely on a past state in the users browser because that also would mean you would have to
-                            send that information through the network to other players, which come back again, to the latency problem.
+                            send that information through the network to other players, which will again, because of network latency.
 
 
 
@@ -44,12 +44,12 @@ class SixthWeekCodesmith extends Component {
                         So now, what do you rely on to predictably display the game correctly on all players screens with no glitches? Time.
                             You calculate what the screen looks like based on how much time has passed. If a player makes an action (like hits the left or right arrows to move),
                             you immediately send that action to other players over the network. Those players then calculate, based on that action and the amount of time passed,
-                            what the game should now look like right now. So rendering takes place based on the interpolation of all the player(s) actions up to the current time.
+                            what the game should now look like right now. Each client does this calculation. So rendering takes place based on the interpolation of all the player(s) actions up to the current time.
                             <br />
                         <br />
                         This is a really cool and advanced topic. I learned a lot about state management in a multiplayer game. Once me and my team
                             firmly grasped this concept, we began to fix the game in singleplayer mode. We scoped the project back and
-                            we did manage to get the game working and interpolating on multiple viewers screens with asteroids floating around.
+                            managed to get the game working and interpolating the game state with multiple views.
 
                             <div className="axteroids-video-outer">
                                 <video width="100%" height="100%" controls autoPlay loop>
