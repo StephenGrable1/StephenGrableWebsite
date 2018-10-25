d001a6786546efa9cc813597a1a518323bdf7ebb
StephenGrable1 <stephengrable@gmail.com>
2018-10-24 23:29:15 -0400

    new build for production

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import "./Contact.css"
 
@@ -9,42 +9,42 @@ class Contact extends React.Component {
     copied: false,
   };
 
-  componentDidMount(){
-    window.scrollTo(0,0);
+  componentDidMount() {
+    window.scrollTo(0, 0);
   }
 
   render() {
     return (
       <div>
         <div className="contact-outer">
-        <div className="contact-inner">
+          <div className="contact-inner">
 
             <div className="question">
               <h2 className="contact-question">How do I request my first quote?</h2>
               <p className="my-email">Please email me at stephengrable@gmail.com and I will get back in touch as soon as possible!</p>
-              
+
               <div className="copy-email-btn">
-              <CopyToClipboard text={this.state.value}
-                  onCopy={() => this.setState({copied: true})}>
+                <CopyToClipboard text={this.state.value}
+                  onCopy={() => this.setState({ copied: true })}>
                   <div className="copy-to-clipboard-btn">
-                  <button>Copy email to clipboard</button>
+                    <button>Copy email to clipboard</button>
                   </div>
                 </CopyToClipboard>
-                {this.state.copied ? <span id="copy-status" style={{color: 'green'}}><p>Copied. Paste Away!</p></span> : null}
+                {this.state.copied ? <span id="copy-status" style={{ color: 'green' }}><p>Copied. Paste Away!</p></span> : null}
               </div>
 
             </div>
             <div className="question two-question">
               <h2 className="contact-question">I need a website, can you help?</h2>
               <p>I can! Let me guide you through the website development process. I'll design and
-              develope a website you can be proud to show to your clients.
+              develop a website you can be proud to show to your clients.
               </p>
             </div>
 
             <div className="question">
-            <h2 className="contact-question">Can you update my current website to something better?</h2>
-            <p>You want your future customers to have a great experience interacting with your site. I can get you there.
-            Let's give your website a brand new look!
+              <h2 className="contact-question">Can you update my current website to something better?</h2>
+              <p>You want your future customers to have a great experience interacting with your site. I can get you there.
+              Let's give your website a brand new look!
             </p>
             </div>
 
